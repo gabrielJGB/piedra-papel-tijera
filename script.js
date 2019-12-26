a44b4c52d2b7f331b788d7a9ccb85019847ed63a
gabrielJGB <brunengo.gabriel@gmail.com>
2019-12-26 00:13:28 -0300

    agrega diseño

---
+++ script.js
@@ -0,0 +1,8 @@
+piedra = document.querySelector('.piedra');
+papel = document.querySelector('.papel');
+tijera = document.querySelector('tijera');
+
+function botonPresionado(e){
+    const $boton = e.target;
+    console.log($boton);
+}
