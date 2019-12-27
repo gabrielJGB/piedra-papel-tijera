471375609d3fb44410f4d3feeec9fd09f8f9a978
gabrielJGB <brunengo.gabriel@gmail.com>
2019-12-27 05:06:21 -0300

    update js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let opcionUsuario = '';
-
-piedra = document.querySelector('.piedra').addEventListener('click',clickPiedra);
-papel = document.querySelector('.papel').addEventListener('click',clickPapel);
-tijera = document.querySelector('.tijera').addEventListener('click',clickTijera);
+let puntaje;
+const piedra = document.querySelector('.piedra').addEventListener('click',clickPiedra);
+const papel = document.querySelector('.papel').addEventListener('click',clickPapel);
+const tijera = document.querySelector('.tijera').addEventListener('click',clickTijera);
+const $jugar = document.querySelector('.jugar');
+$jugar.style.display = "none";
 
 function clickPiedra(){
     opcionUsuario = 'piedra';
@@ -30,15 +32,16 @@ function generarOpcionMaquina(){
 
 function comienzoJuego(){
     const opcionMaquina = generarOpcionMaquina().classList[1];
-    
     manejarResultado(opcionMaquina);
 
     if(opcionUsuario === 'piedra'){
         if(opcionMaquina === 'tijera'){
             console.log('ganaste');
+            puntaje++;
         }
         else if (opcionMaquina === 'papel'){
             console.log('perdiste');
+            puntaje--;
         }
         else{
             console.log('empate');
@@ -48,9 +51,11 @@ function comienzoJuego(){
     if(opcionUsuario === 'papel'){
         if(opcionMaquina === 'piedra'){
             console.log('ganaste');
+            puntaje++;
         }
         else if (opcionMaquina === 'tijera'){
             console.log('perdiste');
+            puntaje--;
         }
         else{
             console.log('empate');
@@ -60,9 +65,11 @@ function comienzoJuego(){
     if(opcionUsuario === 'tijera'){
         if(opcionMaquina === 'papel'){
             console.log('ganaste');
+            puntaje++;
         }
         else if (opcionMaquina === 'piedra'){
             console.log('perdiste');
+            puntaje--;
         }
         else{
             console.log('empate');
@@ -73,14 +80,15 @@ function comienzoJuego(){
 function manejarResultado(opcionMaquina){
     const $botones = document.querySelector('.botones');
     const $resultado = document.querySelector('.resultado');
+    $jugar.style.display = "";
     $botones.style.display = "none";
     const elementoUsuario = document.querySelector('.'+opcionUsuario);
     const elementoMaquina = document.querySelector('.'+opcionMaquina);
 
-    $resultado.append(document.querySelector('.'+opcionUsuario));
-    $resultado.append(document.querySelector('.'+opcionMaquina));
+    $resultado.appendChild(document.querySelector('.'+opcionUsuario));
+    $resultado.appendChild(document.querySelector('.'+opcionMaquina));
     
-
+    console.log(puntaje)
 /*console.log(document.querySelector('.'+opcionUsuario));
 console.log(document.querySelector('.'+opcionMaquina));*/
 
